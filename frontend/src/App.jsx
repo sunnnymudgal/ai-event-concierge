16d80ef83e08dd636d1838d8c07a2d8ff1d1b532
Sunnnymudgal <Sunnyxmudgal@gmail.com>
2026-03-19 23:14:03 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "https://ai-event-backend.onrender.com";
+const API = "https://ai-event-concierge-xxih.onrender.com";
 
 function App() {
   const [input, setInput] = useState("");
